3fb31e4574e4f98f06b77809ac4b9f33e749e6a9
vijayv511 <vijayv511>
2026-06-18 18:34:45 +0530

    changes done in a.py

--- a.py
+++ a.py
@@ -1,19 +1,24 @@
 import os
 import time
+from datetime import datetime
 
 
-timestamp = int(time.time())
-
-print(timestamp)
+#print(timestamp)
+timestamp = datetime.now().strftime("%H:%M:%S %d %b %Y")
 
 import subprocess
 
 def run_script():
     subprocess.run(["bash", "db.sh"], check=True)
 
+def build_app():
+    print(timestamp)
+
 def main():
     print("Pipeline Started")
+    build_app()
     run_script()
+    build_app()
     print("Pipeline Completed")
 
 if __name__ == "__main__":
